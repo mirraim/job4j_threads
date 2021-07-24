2405e9b46e0f854a53828f3c76cda4dba0b6763d
Mirraim <mirraim@gmail.com>
2021-07-24 18:58:22 +0500

    1.1. Состояние нити. [#229175 #181271]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -5,18 +5,16 @@ public class ThreadState {
         Thread first = new Thread(
                 () -> System.out.println(Thread.currentThread().getName())
         );
-        first.start();
+
         Thread second = new Thread(
                 () -> System.out.println(Thread.currentThread().getName())
         );
-        System.out.println(first.getState());
-
+        first.start();
         second.start();
-        while (first.getState() != Thread.State.TERMINATED) {
-            System.out.println(first.getState());
-        }
-        while (second.getState() != Thread.State.TERMINATED) {
-            System.out.println(second.getState());
+        while (first.getState() != Thread.State.TERMINATED
+                || second.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getName() + ":" + first.getState());
+            System.out.println(second.getName() + ":" + second.getState());
         }
         System.out.println("All threads are terminated");
     }
