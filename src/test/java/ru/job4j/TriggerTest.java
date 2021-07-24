de4e2879782f0555f0a6f450e9c8f23b925b05b8
Mirraim <mirraim@gmail.com>
2021-07-24 17:07:42 +0500

    Trigger codecov

---
+++ src/test/java/ru/job4j/TriggerTest.java
@@ -0,0 +1,14 @@
+package ru.job4j;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TriggerTest {
+
+    @Test
+    public void someLogic() {
+        Assert.assertEquals(1,new Trigger().someLogic());
+    }
+}
