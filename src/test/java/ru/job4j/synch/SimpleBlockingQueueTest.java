9d0babe6e34e5e11d580f78680dba2e1b59a7425
Mirraim <mirraim@gmail.com>
2021-08-03 13:16:37 +0500

    3. Junit тест для блокирующей очереди. [#68589]

--- src/test/java/ru/job4j/synch/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/synch/SimpleBlockingQueueTest.java
@@ -2,6 +2,10 @@ package ru.job4j.synch;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.IntStream;
+
 import static org.junit.Assert.*;
 
 public class SimpleBlockingQueueTest {
@@ -51,5 +55,63 @@ public class SimpleBlockingQueueTest {
         produser.join();
         consumer.join();
         assertFalse(queue.isEmpty());
+    }
+
+    @Test
+    public void whenFetchAllThenGetIt() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+        Thread producer = new Thread(
+                () -> IntStream.range(0, 5).forEach(
+                        queue::offer
+                )
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertEquals(buffer, Arrays.asList(0, 1, 2, 3, 4));
+    }
+
+    @Test
+    public void whenFetchAllThenGetItAll() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+        Thread producer = new Thread(
+                () -> Arrays.stream(new int[]{3, 8, 16, 25}).forEach(
+                        queue::offer
+                )
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertEquals(buffer, Arrays.asList(3, 8, 16, 25));
     }
 }
